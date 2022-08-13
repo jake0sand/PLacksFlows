d93bf2ba3827007691dfd79a73c7a881caa78ed9
jake0sand <79411811+jake0sand@users.noreply.github.com>
2022-08-13 16:23:33 -0600

    Initial commit

---
+++ app/src/main/java/com/jakey/placksflows/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.jakey.placksflows
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.jakey.placksflows.ui.theme.PLacksFlowsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val viewModel: MainViewModel by viewModels()
+            val time = viewModel.countDownFlow.collectAsState(initial = "").value
+            PLacksFlowsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        Text(
+                            text = time.toString(),
+                            fontSize = 30.sp,
+                            modifier = Modifier.align(Alignment.Center)
+                            )
+
+                    }
+                }
+            }
+        }
+    }
+}
